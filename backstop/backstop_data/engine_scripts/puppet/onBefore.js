8312e16678b98098bc47b6da424ab4c9ab7a632a
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2022-05-31 16:58:14 +0200

    increase delays before taking screenshots, fix Backstop "Navigation timeout of 60000 ms exceeded"

--- backstop/backstop_data/engine_scripts/puppet/onBefore.js
+++ backstop/backstop_data/engine_scripts/puppet/onBefore.js
@@ -1,3 +1,6 @@
 module.exports = async (page, scenario, vp) => {
   await require('./loadCookies')(page, scenario);
+
+  // fix Backstop "Navigation timeout of 60000 ms exceeded"
+  await page.setDefaultNavigationTimeout(0);
 };
