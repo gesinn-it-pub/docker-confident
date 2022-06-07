5c9fc772dfd737316c5855ba732f00a6dd1f3798
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2022-06-07 17:15:34 +0200

    increased waitForTimeout during login, updated backstop references backup, use ghcr.io/gesinn-it-pub/mediawiki-backup

--- backstop/backstop_data/engine_scripts/puppet/login.js
+++ backstop/backstop_data/engine_scripts/puppet/login.js
@@ -4,6 +4,6 @@ module.exports = async (page, scenario) => {
     await page.type('input#wpName1', 'WikiSysop');
     await page.type('input#wpPassword1', 'wiki4everyone');
     await page.click('button#wpLoginAttempt');
-    await page.waitForTimeout(10000);
+    await page.waitForTimeout(20000);
     await page.goto(scenario.url);
 };
